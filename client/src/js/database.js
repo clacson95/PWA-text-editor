6a3ec6ae04eec6cdbdb6bd99482c3991ff98378b
clacson95 <caleb.lacson@gmail.com>
2022-12-20 19:15:01 -0500

    idb database logic

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,3 +1,4 @@
+// reference: https://www.npmjs.com/package/idb 
 import { openDB } from 'idb';
 
 const initdb = async () =>
@@ -12,10 +13,48 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// accepts content & adds it to the database
+export const putDb = async (content) => {
+  console.log("POST to the database");
+
+  // creates a connection to the database, using a specified version
+  const jateDb = await openDB("jate", 1);
+
+  // creates a new transaction, specifies the database and data privileges
+  const tx = jateDb.transaction("jate", "readwrite");
+
+  // opens the desired object store
+  const store = tx.objectStore("jate");
+
+  // uses ".add()" method on the store, passes in the content
+  const request = store.put({ id: 1, value: content });
+
+  // gets request confirmation
+  const result = await request;
+  console.log("🚀 - data saved to the database", result);
+};
+
+
+// retrieves data from the database
+export const getDb = async () => {
+  console.log("GET from the database");
+
+  // "
+  const jateDb = await openDB("jate", 1);
+
+  const tx = jateDb.transaction("jate", "readonly");
+
+  const store = tx.objectStore("jate");
+
+  // uses ".getAll()" method to get all data in the database
+  const request = store.get(1);
+
+  // gets request confirmation
+  const result = await request;
+  console.log("result.value", result);
+  return result?.value;
+};
+
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,4 @@
 // required plugins
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
